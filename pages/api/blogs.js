6e84d8053fd02c97610d6735940e92cff54b61ad
Aalok Tanwar <aaloktanwar88@gmail.com>
2022-11-21 18:45:42 +0530

    cors error

--- pages/api/blogs.js
+++ pages/api/blogs.js
@@ -1,7 +1,15 @@
 const connectDatabase = require("../../utils/db");
 import Blogs from "../../models/Blogs";
+import NextCors from "nextjs-cors";
 
 export default async function handler(req, res) {
+
+    await NextCors(req, res, {
+      // Options
+      methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
+      origin: "*",
+      optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+    });
 
     connectDatabase();
 
